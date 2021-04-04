6ff678615724ef4545d629749cf0db8618b5585b
jylee <wjddp3@naver.com>
2021-04-04 23:35:41 +0900

    17. Letter Combinations of a Phone Number 풀이

--- Greedy/M-17-Letter Combinations of a Phone Number/JeongYe.js
+++ Greedy/M-17-Letter Combinations of a Phone Number/JeongYe.js
@@ -16,38 +16,20 @@ var letterCombinations = function (digits) {
     9: ["w", "x", "y", "z"],
   };
 
-  const maxSize = digits.length;
-  let index = 1;
-  let combination = digitMap[digits.charAt(0)];
-  if (digits.length === 1) return combination;
+  let result = [];
+  function backtrack(str, index) {
+    if (str.length === digits.length) {
+      result.push(str);
+      return;
+    }
 
-  //파라미터로 넘어온 배열에 현재 인덱스의 알파벳 목록과 1:1 매핑 한다.
-  function recursion() {
-    if (index === maxSize) return;
-
-    const digit = digits.charAt(index);
-    const letters = digitMap[digit];
-
-    let temp = [];
-    letters.forEach((letter) => {
-      combination.forEach((prev) => {
-        temp.push(prev + letter);
-      });
-    });
-
-    index++;
-    combination = temp;
-
-    recursion();
+    //str을 고정 시키고 문자열을 이어 붙인다.
+    for (let letter of digitMap[digits[index]]) {
+      backtrack(str + letter, index + 1);
+    }
   }
 
-  recursion();
-  return combination;
+  backtrack("", 0);
+
+  return result;
 };
-
-//test
-const digits = "9";
-//   const digits = "";
-// const digits = "2";
-
-console.log(letterCombinations(digits));
