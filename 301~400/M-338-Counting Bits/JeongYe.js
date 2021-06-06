865b60d994d062bba174d12ce2546a013b44e624
jylee <wjddp3@naver.com>
2021-06-06 23:34:46 +0900

    338. Counting Bits 풀이2

--- 301~400/M-338-Counting Bits/JeongYe.js
+++ 301~400/M-338-Counting Bits/JeongYe.js
@@ -3,39 +3,9 @@
  * @return {number[]}
  */
 var countBits = function (n) {
-  let result = [];
-
-  for (let i = 0; i <= n; i++) {
-    const binary = decimalToBinary(i);
-    result.push(countOnes(binary));
-  }
+  const result = [...new Array(n + 1)].map(
+    (_, i) => Number(i).toString(2).replace(/0/g, "").length
+  );
 
   return result;
 };
-
-//10진수를 2진수로 변환한다.
-function decimalToBinary(num) {
-  let divider = num;
-  let remainder = 0;
-  let arr = [];
-
-  if (num <= 1) return num + "";
-
-  while (divider !== 1) {
-    remainder = divider % 2;
-    divider = Math.floor(divider / 2);
-    arr.push(remainder);
-  }
-
-  arr.push(divider);
-
-  return arr.reverse().join("");
-}
-
-// 문자열로된 이진수 안에 1을 카운팅한다. 
-function countOnes(num) {
-  let count = 0;
-  Array.from(num).forEach((n) => n == 1 && count++);
-
-  return count;
-}
